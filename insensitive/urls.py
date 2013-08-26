b6f10153446d46aa975d39da60052395097143e1
Asser Schrøder Femø <asser@diku.dk>
2013-08-26 11:07:11 +0200

    Update url imports for django 1.4 and later

--- insensitive/urls.py
+++ insensitive/urls.py
@@ -3,7 +3,10 @@ Created on 2011-06-12
 
 @author: George
 '''
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls import patterns, url
+except ImportError:  # django < 1.4
+    from django.conf.urls.defaults import patterns, url
 from insensitive.views import login
 
 urlpatterns = patterns('',
